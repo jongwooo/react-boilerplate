db5ad79e549e5ef63591e6741e851fe8d300bb82
jongwooo <han980817@gmail.com>
2022-02-02 16:05:16 +0900

    feat: Create src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.png'
+
+const App = () => {
+    return(
+        <div className="App">
+            <h1>Hello, World!</h1>
+            <img src={logo}  alt="logo"/>
+        </div>
+    )
+}
+
+export default App

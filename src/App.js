67826d51361b93d4b5b77f562d8c958dbb619341
jongwooo <han980817@gmail.com>
2022-02-03 17:27:43 +0900

    feat: Enable hot module replacement (#3)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { hot } from 'react-hot-loader'
 import logo from './logo.png'
 
 const App = () => {
@@ -10,4 +11,4 @@ const App = () => {
     )
 }
 
-export default App
+export default hot(module)(App)
